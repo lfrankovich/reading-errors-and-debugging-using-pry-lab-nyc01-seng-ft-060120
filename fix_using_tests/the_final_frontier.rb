728280a4acf31c4b8d096618327d19c32c608fae
Lauren Frankovich <frankovister@gmail.com>
2020-05-22 19:21:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,5 +10,9 @@ end
 def engage
   puts state_log(date)
   date = generate_star_date
-  binding.pry
 end
+engage
+
+describe "the_final_frontier" do
+  it "`engage` should return the captain's log and crew greetings" do
+    expect{ engage }.to output(/Captain's Log, star date /).to_stdout
