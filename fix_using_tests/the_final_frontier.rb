05cfc7467eb054b1ba7f520f6cae696833e6a89e
Lauren Frankovich <frankovister@gmail.com>
2020-05-22 19:28:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,12 +1,13 @@
 def generate_star_date
-  (rand(100000.0) + 400000.0) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
+
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
+  puts state_log(date)
   date = generate_star_date
-  state_log(date)
 end
